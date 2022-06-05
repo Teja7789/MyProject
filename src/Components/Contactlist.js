c2586d5d67a302ea07f975d6a2f21161731dc94d
Teja7789 <rx100789@gmail.com>
2022-06-05 20:44:30 +0530

    1 file modified

--- src/Components/Contactlist.js
+++ src/Components/Contactlist.js
@@ -2,9 +2,22 @@ import React from 'react';
 //props used pass contacts array
 const Contactlist = (props) => {
 // console.log(props)
+const renderContactlist = props.contacts.map((contacts)=>{
+  return(
+    <div class="item">
+      <div className="content">
+        <div className="header">
+          {contacts.name}</div>
+          <div>{contacts.email}</div>
+          {/* <div>Remove</div> */}
+      </div>
+      <i className="trash-alternate-outline-icon">delete</i>
+    </div>
+  )
+})
   return(
     <div className="ui celled list">
-     Contactlist
+     {renderContactlist}
       </div>
 
   );
